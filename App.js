f8cfe1dbbb1d8e27722642294877f58a4f811948
Jason Ruhlin <jsonruhls@users.noreply.github.com>
2025-01-18 15:20:42 -0500

    feat: Apply drawer navigation to app

--- App.js
+++ App.js
@@ -1,8 +1,13 @@
 import React from 'react';
-import AppNavigator from './navigation/AppNavigator';
+import {NavigationContainer} from '@react-navigation/native';
+import DrawerNavigator from './src/navigation/DrawerNavigator';
 
 const App = () => {
-    return <AppNavigator />;
+	return (
+		<NavigationContainer>
+			<DrawerNavigator />
+		</NavigationContainer>
+	);
 };
 
 export default App;
